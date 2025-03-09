dd9d6a0e9787c582a99b75efc2086bf3334ae713
Talha <p240050@pwr.nu.edu.pk>
2025-03-09 05:01:14 +0500

    Talha Zaheer 3 March 2025 4:57 am feature branch

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 print ('This is Versioning Practically')
+
+print ('This is Versioning Practically')
+print ("Now Git will become my new skill set but I won't do chichorapan on LinkedIn 😊 but add it as a skill")
